cb761251e4af7a238c6887ac14bc23c0ad0bd768
Tom Sherman <the.tomsherman@gmail.com>
2023-06-02 09:50:40 +0100

    Remove console log

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const session = parseSessionCookie(request.cookies);
-  console.log(session);
 
   if (!session) {
     return NextResponse.redirect(new URL("/login", request.nextUrl).toString());
