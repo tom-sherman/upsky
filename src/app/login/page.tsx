4f31d27cd3c839d6622798d2215ce646b13270c2
Tom Sherman <the.tomsherman@gmail.com>
2023-06-02 09:56:02 +0100

    Scaffold login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,21 @@
 export default function LoginPage() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <h1>Login</h1>
+      <form action={loginAction}>
+        <label>
+          Username:
+          <input type="text" />
+        </label>
+        <label>
+          Password:
+          <input type="password" />
+        </label>
+      </form>
+    </>
+  );
+}
+
+async function loginAction(formData: FormData) {
+  "use server";
 }
